01251b84ce9929dd4a736032c801504ef2120756
Dmytro <dmytro.nakonn@gmail.com>
2025-10-16 19:24:49 +0300

    init pydantic schemas

--- api/site_generation/models.py
+++ api/site_generation/models.py
@@ -1,4 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sqlalchemy import Column
+from uuid import UUID
+import datetime
 
 
+class GenerateBody(BaseModel):
+    topic: str = Field(..., min_length=1, max_length=150)  # forbid too long text requests
+    pages_count: int = Field(..., ge=1, le=25)
+    style: str = Field(..., min_length=1, max_length=150)
+    max_tokens: int = Field(..., ge=1, le=100_000)
+    
+    
+class SiteMetaData:
+    id: UUID  # Id of the site
+    title: str
+
+
+# Gonna returns titles and site ids (metadata) for later access
+class GenerateResponse(BaseModel):
+    topic: str
+    style: str
+    max_tokens: int
+    pages_count: int
+    id: UUID  # Request ID
+    sites: list[SiteMetaData]
+    time: datetime.datetime
