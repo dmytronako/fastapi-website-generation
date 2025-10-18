c798add4d10770b5c5bf212a2d274455360ff55f
Dmytro <dmytro.nakonn@gmail.com>
2025-10-18 16:42:57 +0300

    fix dockerfile

--- api/site_generation/service.py
+++ api/site_generation/service.py
@@ -19,3 +19,9 @@ async def create_generate_request(
     await async_db_session.commit()
     await async_db_session.refresh(generate)
     logger.debug(f'Generate request {generate.id} put into DB')
+
+
+async read_generate_requests(
+    async_db_session: AsyncSession
+) -> list[]:
+    
